9d597b2c4c8178aa83c8a2f8a854ecb7e0cae1af
Lukasz Kolodziejczyk <lukasz.m.kolodziejczyk@gmail.com>
2025-04-10 21:37:57 +0000

    mlc test weigts

--- spikes/mlc-llm/batch.py
+++ spikes/mlc-llm/batch.py
@@ -5,6 +5,15 @@ from mlc_llm.serve import AsyncMLCEngine
 from mlc_llm.serve.config import EngineConfig
 from pydantic import BaseModel
 import json
+from mlc_llm.interface.convert_weight import convert_weight
+from pathlib import Path
+from mlc_llm.quantization import QUANTIZATION
+from mlc_llm.model import MODELS
+from mlc_llm.support.auto_config import detect_model_type, detect_config
+from mlc_llm.support.auto_device import detect_device
+from mlc_llm.support.auto_weight import detect_weight
+
+from typing import Union
 
 # mlc-llm works with weights in MLC format
 # model = "HF://mlc-ai/Qwen2.5-0.5B-Instruct-q4f16_1-MLC"
@@ -15,7 +24,63 @@ prompts = [' {"question": "What is the meaning of life?"}'] * n_requests
 class Answer(BaseModel):
     answer: str
 
+def _convert_weights(
+    config: str,
+    quantization: str = "q0f16",  # e.g. "q4f16_1" (see QUANTIZATION)
+    model_type: str = "auto",  # e.g. "mistral" (see MODELS)
+    device: str = "auto",
+    source: str = "auto",
+    source_format: str = "auto",  # "auto", "huggingface-torch", "huggingface-safetensor", "awq"
+    output_path: str = "out_mlc",
+):
+    t0 = time.time()
+
+    def _parse_source(path: Union[str, Path], config_path: Path) -> Path:
+        if path == "auto":
+            return config_path.parent
+        path = Path(path)
+        if not path.exists():
+            raise argparse.ArgumentTypeError(f"Model source does not exist: {path}")
+        return path
+
+    def _parse_output(path: Union[str, Path]) -> Path:
+        path = Path(path)
+        if not path.is_dir():
+            path.mkdir(parents=True, exist_ok=True)
+        return path
+
+    config = detect_config(config)
+    quantization = QUANTIZATION[quantization]
+    output_path = _parse_output(output_path)
+    device = detect_device(device)
+    source, source_format = detect_weight(
+        weight_path=_parse_source(source, config),
+        config_json_path=config,
+        weight_format=source_format,
+    )
+    model = detect_model_type(model_type, config)
+    convert_weight(
+        config=config,
+        quantization=quantization,
+        model=model,
+        device=device,
+        source=source,
+        source_format=source_format,
+        output=output_path
+    )
+    t1 = time.time()
+    print(f"Conversion time: {t1 - t0:.2f} seconds")
+    return
+
 async def main():
+    test_convert_weights = False
+    if test_convert_weights:
+        # make sure to do before:
+        # > git lfs install
+        # > git clone https://huggingface.co/Qwen/Qwen2.5-1.5B
+        _convert_weights("Qwen2.5-1.5B/config.json")
+        return
+
     engine = AsyncMLCEngine(
         model=model,
         mode="server", # "local", "interactive", "server"
